5434c7aca0f51bbaf19d162e90f18c6fe0dfdf47
suyashpradhan <pradhansuyash39@gmail.com>
2021-01-31 23:15:49 +0530

    Lifecycle Methods

---
+++ src/Lifecycle Methods/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+import "../App.css";
+
+class App extends Component {
+  constructor() {
+    super();
+    this.setState = {};
+  }
+  render() {
+    return (
+      <div>
+        <h1>Lifecycle Methods</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import List from "./List";
+import App from "./Lifecycle Methods/App";
 
 ReactDOM.render(
   <React.StrictMode>
-    <List />
+    <App />
   </React.StrictMode>,
   document.getElementById("root")
 );
